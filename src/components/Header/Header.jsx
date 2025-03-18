085f68503d79e2d025793f8fb3ce02363c744ebb
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-03-18 21:44:46 +0100

    implement header for mobile version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import { MainLogo } from '../MainLogo'
+import { ThemeToggle } from '../UI/ThemeToggle/ThemeToggle'
 import './Header.scss'
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <MainLogo />
+        <div className="header__content">
+          <MainLogo />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   )
---
+++ src/components/UI/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,5 @@
+import './ThemeToggle.scss'
+
+export const ThemeToggle = () => {
+  return <button className="theme-toggle-button dark-mode bgd-img" />
+}
